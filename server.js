f9f41534820268d2402ca64d45b9a15141866809
IustinApetre <apetre.i@yahoo.com>
2024-03-03 10:33:50 +0200

    Fix: bcrypt error fixed

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //mongodb
 require(`./config/db`);
 
-const app = require (`express`)();
-const port = 3000;
-const UserRouter= require(`./api/User`)
+const app = require(`express`)();
+const port = process.env.PORT || 3000;
+
+const UserRouter = require(`./api/User`)
 // For accepting post form data
-const bodyParser = require ( `express`).json;
+const bodyParser = require(`express`).json;
 app.use(bodyParser());
 app.use(`/user`, UserRouter);
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
